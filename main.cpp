7240847e58ac63ff553f3c7b26d6c35fc51a4185
Matthias <hias.fuchs@gmail.com>
2020-04-28 21:57:45 +0200

    refactor for windows

--- main.cpp
+++ main.cpp
@@ -7,19 +7,22 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#include "SerialConfig.h"
 
 #endif
 
 static volatile int keepRunning;
-static volatile int* ptrRunning;
+static volatile int *ptrRunning;
 
-void intHandler(int dummy) {
+void intHandler(int dummy)
+{
     keepRunning = 0;
     ptrRunning = &keepRunning;
 }
 
 int main(int, char **)
 {
+    printf("serial %d.%d.%d \n", SERIAL_VERSION_MAJOR , SERIAL_VERSION_MINOR, SERIAL_VERSION_PATCH );
     keepRunning = 1;
     ptrRunning = &keepRunning;
     signal(SIGINT, intHandler);
@@ -28,4 +31,3 @@ int main(int, char **)
     //dataStart();
     dataClean();
 }
-
