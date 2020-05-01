207d5059e984b31b85f42cd5aed9e822e189b55d
Matthias <hias.fuchs@gmail.com>
2020-05-01 17:05:06 +0200

    ;

--- sender/send.cpp
+++ sender/send.cpp
@@ -80,7 +80,7 @@ int send(char *SendByte)
     int res;
 
     printf("    %s\n", SendByte);
-    char endstring[1] = {0x3B};
+    char endstring[2] = ";";
 
     res = write(fd, SendByte, strlen(SendByte));
 
