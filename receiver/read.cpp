a61b5c617d700338c00610a4a15676f86416f641
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2020-09-26 11:02:50 +0200

    correct

--- receiver/read.cpp
+++ receiver/read.cpp
@@ -11,8 +11,6 @@
 #include <string.h>
 #include <string>
 
-
-
 // Linux headers
 #include <errno.h> // Error integer and strerror() function
 #include <unistd.h>
@@ -49,63 +47,62 @@ int read(char *portname, volatile int *running)
     //fd = open(portname, O_RDONLY);
     fd = open(portname, O_RDWR | O_NOCTTY | O_NDELAY);
 
-    
     if (fd < 0)
     {
         perror(portname);
         return 1;
     }
 
-    tcgetattr(fd, &oldtio); //save current port settings 
+    tcgetattr(fd, &oldtio); //save current port settings
     bzero(&newtio, sizeof(newtio));
 
     // new
     // https://www.linuxquestions.org/questions/programming-9/why-minicom-reads-data-correctly-from-dev-ttyusb0-but-my-program-does-not-904382/
-    
+
     flag = fcntl(fd, F_GETFL, 0);
-	fcntl(fd, F_SETFL, flag & ~O_NDELAY);
+    fcntl(fd, F_SETFL, flag & ~O_NDELAY);
 
     //set baudrate to 0 and go back to normal
-	printf("set baudrate to 0......\n");
-	tcgetattr(fd, &newtio);
-	tcgetattr(fd, &oldtio);
-	cfsetospeed(&newtio, B0);
-	cfsetispeed(&newtio, B0);
-	tcsetattr(fd, TCSANOW, &newtio);
-	sleep(1);
-	tcsetattr(fd, TCSANOW, &oldtio);
-	printf("baudrate is back to normal......\n");
+    printf("set baudrate to 0......\n");
+    tcgetattr(fd, &newtio);
+    tcgetattr(fd, &oldtio);
+    cfsetospeed(&newtio, B0);
+    cfsetispeed(&newtio, B0);
+    tcsetattr(fd, TCSANOW, &newtio);
+    sleep(1);
+    tcsetattr(fd, TCSANOW, &oldtio);
+    printf("baudrate is back to normal......\n");
 
-	tcgetattr(fd, &newtio);
+    tcgetattr(fd, &newtio);
 
-	baudrate = B9600;
-	cfsetospeed(&newtio, baudrate);
-	cfsetispeed(&newtio, baudrate);
+    baudrate = B9600;
+    cfsetospeed(&newtio, baudrate);
+    cfsetispeed(&newtio, baudrate);
 
     newtio.c_cflag = (newtio.c_cflag & ~CSIZE) | CS8;
-	
-	//set into raw, no echo mode
-	newtio.c_iflag = ~IGNBRK;
+
+    //set into raw, no echo mode
+    newtio.c_iflag = ~IGNBRK;
     newtio.c_iflag &= BRKINT;
     //newtio.c_iflag = ICRNL;
 
-	newtio.c_lflag = 0;
-	newtio.c_oflag = 0;
-	newtio.c_cflag |= CLOCAL | CREAD;
+    newtio.c_lflag = 0;
+    newtio.c_oflag = 0;
+    newtio.c_cflag |= CLOCAL | CREAD;
 
-	newtio.c_cflag &= ~CRTSCTS;
+    newtio.c_cflag &= ~CRTSCTS;
 
-	//turn off software control
-	newtio.c_iflag &= ~(IXON | IXOFF | IXANY);
+    //turn off software control
+    newtio.c_iflag &= ~(IXON | IXOFF | IXANY);
 
-	//no parity
-	newtio.c_cflag &= ~(PARENB | PARODD);
+    //no parity
+    newtio.c_cflag &= ~(PARENB | PARODD);
 
-	//1 stopbit
-	newtio.c_cflag &= ~CSTOPB;
-    
-    newtio.c_cc[VTIME] = 1; // inter-character timer unused 
-    newtio.c_cc[VMIN] = 0;  // blocking read until 5 chars received 
+    //1 stopbit
+    newtio.c_cflag &= ~CSTOPB;
+
+    newtio.c_cc[VTIME] = 1; // inter-character timer unused
+    newtio.c_cc[VMIN] = 0;  // blocking read until 5 chars received
 
     tcflush(fd, TCIFLUSH);
     tcsetattr(fd, TCSANOW, &newtio);
@@ -128,14 +125,12 @@ int read(char *portname, volatile int *running)
             outputnr = 0;
             time_t now;
             time(&now);
-            printf("read %s %s" , portname, ctime(&now));
+            printf("read %s %s", portname, ctime(&now));
         }
 #endif
 
         for (int i = 0; i < res; i++)
         {
-
-
 
             if ((buf[i] & COLORADO_ADDRESS_WORD_MASK) == COLORADO_ADDRESS_WORD_MASK)
             {
@@ -145,21 +140,25 @@ int read(char *portname, volatile int *running)
                 g = 0;
             }
 
-#ifdef debug_incoming
-            // printf("%d: %02x ", g, buf[i]);
-            printf("%02x ",buf[i]);
-#endif            
-
             if (buf[i] == COLORADO_ERROR_CR)
             {
                 printf("\n %02x b1 0d \n", COLORADO_ERROR_CR);
                 putReadData(0xb1);
                 putReadData(0x0d);
-            } else {
+#ifdef debug_incoming
+                // printf("%d: %02x ", g, buf[i]);
+                printf("b1 0d ");
+#endif
+            }
+            else
+            {
                 putReadData(buf[i]);
+#ifdef debug_incoming
+                // printf("%d: %02x ", g, buf[i]);
+                printf("%02x ", buf[i]);
+#endif
             }
 
-            
             g++;
         }
         if (!*running)
