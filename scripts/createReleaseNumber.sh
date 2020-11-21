5f815f91839b90b822e8c2834e36337503a0f5be
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2020-11-21 12:55:54 +0100

    Incrementing version number to v0.1.0

--- scripts/createReleaseNumber.sh
+++ scripts/createReleaseNumber.sh
@@ -47,7 +47,7 @@ gh release upload $versionLabel /tmp/windows_binaries_$versionLabel.zip#windows
 rm /tmp/windows_binaries_$versionLabel.zip
 
 zip /tmp/mac_executable_$versionLabel.zip build/serial 
-gh release upload $OSTYPE_$versionLabel /tmp/mac_executable_$versionLabel#mac
+gh release upload $OSTYPE_$versionLabel /tmp/mac_executable_$versionLabel.zip#mac
 rm /tmp/mac_executable_$versionLabel.zip
 
 exit 0
