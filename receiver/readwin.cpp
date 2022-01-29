aa0bf386c8ebce9ff050d2705a63ab34f00a8aee
max <max@home>
2022-01-29 21:46:37 +0100

    serial forward

--- receiver/readwin.cpp
+++ receiver/readwin.cpp
@@ -33,6 +33,111 @@ void PrintCommState(DCB dcb)
              dcb.StopBits);
 }
 
+int setDestSerialPort(HANDLE *hComm, const char *comport_name)
+{
+    DCB dcbSerialParams = {0};
+    BOOL Status; // Status
+    COMMTIMEOUTS timeouts = {0};
+    HANDLE newhComm;
+
+    newhComm = CreateFileA(comport_name,
+                           GENERIC_READ | GENERIC_WRITE,
+                           0,    /* no share  */
+                           NULL, /* no security */
+                           OPEN_EXISTING,
+                           0,     /* no threads */
+                           NULL); /* no templates */
+
+    if (newhComm == INVALID_HANDLE_VALUE)
+    {
+        printf_s("Port can't be opened\n");
+        printf_s("waiting ...\n");
+        Sleep(5000);
+        printf_s("try again\n\n");
+        return 1;
+    }
+
+    //  Initialize the DCB structure.
+    SecureZeroMemory(&dcbSerialParams, sizeof(DCB));
+    dcbSerialParams.DCBlength = sizeof(DCB);
+
+    Status = GetCommState(newhComm, &dcbSerialParams); //retreives  the current settings
+
+    if (Status == FALSE)
+    {
+        printf_s("\nError to Get the Com state\n\n");
+        return 1;
+    }
+
+    printf("init Parameters");
+
+    PrintCommState(dcbSerialParams);
+
+    dcbSerialParams.BaudRate = CBR_9600;   //BaudRate = 9600
+    dcbSerialParams.ByteSize = 8;          //ByteSize = 8
+    dcbSerialParams.Parity = EVENPARITY;   //Parity = None NOPARITY
+    dcbSerialParams.StopBits = ONESTOPBIT; //StopBits = 1
+
+    Status = SetCommState(newhComm, &dcbSerialParams);
+
+    if (Status == FALSE)
+    {
+        printf_s("\nError to Setting DCB Structure\n\n");
+        return 1;
+    }
+
+    printf("set colorado Parameters (9600, ByteSize = 8, Parity = 2, StopBits = 0)");
+    Status = GetCommState(newhComm, &dcbSerialParams); //retreives  the current settings
+
+    if (Status == FALSE)
+    {
+        printf_s("\nError to Get the Com state\n\n");
+        return 1;
+    }
+
+    PrintCommState(dcbSerialParams);
+
+    //Setting Timeouts
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 50;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    timeouts.WriteTotalTimeoutConstant = 1;
+    timeouts.WriteTotalTimeoutMultiplier = 1;
+
+    if (SetCommTimeouts(newhComm, &timeouts) == FALSE)
+    {
+        printf_s("\nError to Setting Time outs");
+        return 1;
+    }
+
+    //Setting Receive Mask
+    Status = SetCommMask(newhComm, EV_RXCHAR);
+    if (Status == FALSE)
+    {
+        printf_s("\nError to in Setting CommMask\n\n");
+        return 1;
+    }
+
+    int int_i = 1;
+    DWORD WrittenBytes;
+
+    if (!WriteFile(newhComm, &int_i, sizeof(int_i), &WrittenBytes, NULL))
+    {
+        printf("...\n");
+        printf("error write\n");
+        printf("...\n");
+        return 1;
+    }
+    else
+    {
+        printf("write success\n");
+    }
+
+    *hComm = newhComm;
+
+    return 0;
+}
+
 //int read(char *portname, volatile int *running, bool verbose)
 int read(char *portname, char *dstname, bool forward, volatile int *running, bool verbose)
 {
@@ -42,6 +147,12 @@ int read(char *portname, char *dstname, bool forward, volatile int *running, boo
     memcpy(subbuff, &portname[3], 2);
     int portnumber = atoi(subbuff);
     int comport_number = portnumber - 1;
+
+    // dest
+    char destbuff[2];
+    memcpy(destbuff, &dstname[3], 2);
+    int dstnumber = atoi(destbuff);
+    int comdst_number = dstnumber - 1;
 
     const char *comports[RS232_PORTNR] = {"\\\\.\\COM1", "\\\\.\\COM2", "\\\\.\\COM3", "\\\\.\\COM4",
                                           "\\\\.\\COM5", "\\\\.\\COM6", "\\\\.\\COM7", "\\\\.\\COM8",
@@ -53,8 +164,11 @@ int read(char *portname, char *dstname, bool forward, volatile int *running, boo
                                           "\\\\.\\COM29", "\\\\.\\COM30", "\\\\.\\COM31", "\\\\.\\COM32"};
 
     //char mode_str[128];
-    HANDLE hComm; // Handle to the Serial port
-    BOOL Status;  // Status
+
+    int ERROR_COUNTER = 1000;
+    HANDLE hComm;    // Handle to the Serial port
+    HANDLE dsthComm; // Handle to the Serial port
+    BOOL Status;     // Status
     OVERLAPPED o;
     DCB dcbSerialParams = {0};   // Initializing DCB structure
     COMMTIMEOUTS timeouts = {0}; //Initializing timeouts structure
@@ -63,6 +177,7 @@ int read(char *portname, char *dstname, bool forward, volatile int *running, boo
     //char ReadData;               //temperory Character
     uint8_t ReadData;
     DWORD NoBytesRead; // Bytes read by ReadFile()
+    DWORD WrittenBytes;
     unsigned char loop = 0;
     wchar_t pszPortName[10] = {0}; //com port id
     wchar_t PortNo[20] = {0};      //contain friendly name
@@ -179,6 +294,12 @@ int read(char *portname, char *dstname, bool forward, volatile int *running, boo
         bool connectsuccess = true;
         printf("receiver - set Serial port %s\n", comports[comport_number]);
 
+        if (forward)
+        {
+            printf("sender - set Serial port %s\n", comports[comdst_number]);
+            setDestSerialPort(&dsthComm, comports[comdst_number]);
+        }
+
         do
         {
             if (WaitCommEvent(hComm, &dwEventMask, &o))
@@ -189,6 +310,20 @@ int read(char *portname, char *dstname, bool forward, volatile int *running, boo
                     do
                     {
                         Status = ReadFile(hComm, &ReadData, sizeof(ReadData), &NoBytesRead, NULL);
+
+                        if (forward)
+                        {
+
+                            if (!WriteFile(dsthComm, &ReadData, sizeof(ReadData), &WrittenBytes, NULL))
+                            {
+                                ERROR_COUNTER++;
+                                if (ERROR_COUNTER > 1000)
+                                {
+                                    printf("error serial write on %s\n", comports[comdst_number]);
+                                    ERROR_COUNTER = 0;
+                                }
+                            }
+                        }
 
                         if ((ReadData & COLORADO_ADDRESS_WORD_MASK) == COLORADO_ADDRESS_WORD_MASK)
                         {
